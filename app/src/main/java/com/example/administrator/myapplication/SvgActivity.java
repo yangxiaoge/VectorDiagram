e9f202cc7247c795e838f4096fe3a0d1ed755397
yangxiaoge <364804051@qq.com>
2016-05-22 22:30:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication/SvgActivity.java
@@ -0,0 +1,12 @@
+package com.example.administrator.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SvgActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_svg);
+    }
+}
